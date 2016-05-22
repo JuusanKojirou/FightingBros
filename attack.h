5ff690559f55221b1dcd841a1a29c5657557da43
Zachery1993 <xy6265878@163.com>
2016-05-22 11:20:54 +0800

    Add files via upload

--- attack.h
+++ attack.h
@@ -6,15 +6,16 @@ enum attack_type
 {
 	melee,remote,magic
 };
+//攻击所带buff，减速，流血，晕眩，燃烧
 enum attack_buff{
 	slower,blood,stun,burn
 };
 struct ATattack_state{
 	attack_type type;
 	attack_buff* buff;
-	int owner;
-	int range;
-	int duration;
+	int owner;//攻击发出者
+	int range;//攻击范围
+	int duration;//攻击持续时间，近战和技能
 };
 class ATattack:public ATelements{
 private:
@@ -22,7 +23,7 @@ private:
 	int range;
 	int duration;
 	attack_buff *buff;
-	int owner;
+	int owner;//判定伤害时不伤害发出攻击的人
 public:
 	ATattack(AToriginal_state state,ATattack_state attack);//返回ATattack类，包括所有信息
 };
---
+++ procedures.h
@@ -0,0 +1,10 @@
+#ifndef PROCEDURES_H
+#define PROCEDURES_H
+#include <vector>
+#include <list>
+#include <string>
+#include "attack.h"
+#include "different_unit.h"
+#include "character.h"
+
+#endif // !PROCEDURES_H
