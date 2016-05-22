0efde2b0a22b42585a714b110656c887338b6e48
Zachery1993 <xy6265878@163.com>
2016-05-22 14:12:58 +0800

    Add files via upload

--- units.h
+++ units.h
@@ -7,9 +7,10 @@ private:
 	int size;
 	int HP, max_HP;
 	int portrait;
-	attack_buff* buff;
+	attack_buff *buff;//buff的大小和总的buff种类一致，每个里面存buff的持续时间，buff不存在用-1表示；
 public:
 	ATunit(AToriginal_state state,int ori_size,int ori_HP,int ori_portrait);
-	virtual int unit_be_hurt(int damage,attack_buff* buff);
+	int unit_be_hurt(int damage,attack_buff buff);
+	void unit_check_state();//如果buff=0，change_mode=RESTORE，调用element_change函数
 };
 #endif // !UNITS_H
