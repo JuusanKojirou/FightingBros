eab3d53545ced62d1d329ecc7952d5cc9790e0bb
Zachery1993 <xy6265878@163.com>
2016-05-24 22:41:24 +0800

    Add files via upload

---
+++ procedures.cpp
@@ -0,0 +1,105 @@
+#include "procedures.h"
+
+ATjudgement::ATjudgement(ATmap initial_map){
+	ATcharacter* temp;
+	for (auto i = initial_map.load_charater.begin(); i != initial_map.load_charater.end(); ++i)
+		 character_information.push_back(*i);
+	for (auto i = initial_map.load_map.begin(); i != initial_map.load_map.end(); ++i)
+		obstacle_information.push_back(*i);
+	unit_ID = 5;
+
+}
+int* ATjudgement::judgement_map_edge(int *pp, int size){
+		if (pp[ROLE_X] > max_x - size)
+			pp[ROLE_X] = max_x - size;
+		if (pp[ROLE_X] < size)
+			pp[ROLE_X] = size;
+		if (pp[ROLE_Y] > max_y - size)
+			pp[ROLE_Y] = max_y - size;
+		if (pp[ROLE_Y] < size)
+			pp[ROLE_Y] = size;
+		return pp;
+}
+
+int* ATjudgement::judgement_rectangle(ATmap_obstacle rectangle_obstacle, int *pp, int size){
+	double temp_x, temp_y, r;
+	if ((pp[ROLE_X] <= rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X] - size || pp[ROLE_X] >= rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X] + size) && (pp[ROLE_Y] <= rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y] - size || pp[ROLE_Y] >= rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y] + size))
+		return pp;
+	if (pp[ROLE_X] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X] && pp[ROLE_Y] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y]){
+		r = sqrt((pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X])*(pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X]) + (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y])*(pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y]));
+		temp_x = (pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X])*size / r + RECTANGLE_MIN_X;
+		temp_y = (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y])*size / r + RECTANGLE_MIN_Y;
+		pp[ROLE_X] = (int)temp_x;
+		pp[ROLE_Y] = (int)temp_y;
+		return pp;
+	}
+	if (pp[ROLE_X] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X] && pp[ROLE_Y] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y]){
+		r = sqrt((pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X])*(pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X]) + (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y])*(pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y]));
+		temp_x = (pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X])*size / r + RECTANGLE_MAX_X;
+		temp_y = (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y])*size / r + RECTANGLE_MIN_Y;
+		pp[ROLE_X] = (int)temp_x;
+		pp[ROLE_Y] = (int)temp_y;
+		return pp;
+	}
+	if (pp[ROLE_X] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X] && pp[ROLE_Y] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y]){
+		r = sqrt((pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X])*(pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X]) + (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y])*(pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y]));
+		temp_x = (pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X])*size / r + RECTANGLE_MIN_X;
+		temp_y = (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y])*size / r + RECTANGLE_MAX_Y;
+		pp[ROLE_X] = (int)temp_x;
+		pp[ROLE_Y] = (int)temp_y;
+		return pp;
+	}
+	if (pp[ROLE_X] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X] && pp[ROLE_Y] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y]){
+		r = sqrt((pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X])*(pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X]) + (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y])*(pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y]));
+		temp_x = (pp[ROLE_X] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X])*size / r + RECTANGLE_MAX_X;
+		temp_y = (pp[ROLE_Y] - rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y])*size / r + RECTANGLE_MAX_Y;
+		pp[ROLE_X] = (int)temp_x;
+		pp[ROLE_Y] = (int)temp_y;
+		return pp;
+	}
+	if (pp[ROLE_X] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_X]){
+		pp[ROLE_X] = RECTANGLE_MIN_X - size;
+		return pp;
+	}
+	if (pp[ROLE_X] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_X]){
+		pp[ROLE_X] = RECTANGLE_MAX_X + size;
+		return pp;
+	}
+	if (pp[ROLE_Y] < rectangle_obstacle.shape_paramenter[RECTANGLE_MIN_Y]){
+		pp[ROLE_Y] = RECTANGLE_MIN_Y - size;
+		return pp;
+	}
+	if (pp[ROLE_Y] > rectangle_obstacle.shape_paramenter[RECTANGLE_MAX_Y]){
+		pp[ROLE_Y] = RECTANGLE_MAX_Y + size;
+		return pp;
+	}
+	
+}
+
+int* ATjudgement::judgement_circle(ATmap_obstacle circle_obstacle, int *pp, int size){
+	double temp_x, temp_y, r;
+	r = sqrt((pp[ROLE_X] - circle_obstacle.shape_paramenter[CIRCLE_X])*(pp[ROLE_X] - circle_obstacle.shape_paramenter[CIRCLE_X]) + (pp[ROLE_Y] - circle_obstacle.shape_paramenter[CIRCLE_Y])*(pp[ROLE_Y] - circle_obstacle.shape_paramenter[CIRCLE_Y]));
+	if (r < circle_obstacle.shape_paramenter[CIRCLE_R]+size){
+		temp_x = (pp[ROLE_X] - circle_obstacle.shape_paramenter[CIRCLE_X])*(size + circle_obstacle.shape_paramenter[CIRCLE_R]) / r + circle_obstacle.shape_paramenter[CIRCLE_X];
+		temp_y = (pp[ROLE_Y] - circle_obstacle.shape_paramenter[CIRCLE_Y])*(size + circle_obstacle.shape_paramenter[CIRCLE_R]) / r + circle_obstacle.shape_paramenter[CIRCLE_Y];
+		pp[ROLE_X] = (int)temp_x;
+		pp[ROLE_Y] = (int)temp_y;
+
+	}
+	return pp;
+}
+void ATjudgement::judgement_get_signal(int character_ID, int signal_type, int value_1, double value_2){
+	for (auto i = character_information.begin(); i != character_information.end();++i)
+	if ((*i).element_get_ID==character_ID)
+		switch (signal_type){
+		case ROTATE_CIR:{(*i).element_change_direction(value_2);}
+		case ROTATE_ANTICIR:{(*i).element_change_direction(-value_2); }
+		case RELEASE_ATTACK:{judgement_add_attack((*i).hero_attack()); }
+	}
+
+}
+void ATjudgement::judgement_check_position(){
+	for
+
+}
+
--- procedures.h
+++ procedures.h
@@ -1,22 +1,37 @@
 #ifndef PROCEDURES_H
 #define PROCEDURES_H
 #define SHAPE_RECTANGLE 4
-#define SHAPE_CIRCLE 2
+#define SHAPE_CIRCLE 3
 //signal type
 #define ROTATE_CIR 1
-#define	ROTATE_ANTICIR 2
+#define	ROTATE_ANTICIR -1
 #define RELEASE_ATTACK 3
+#define RECTANGLE_MIN_X 0
+#define	RECTANGLE_MAX_X 1
+#define RECTANGLE_MIN_Y 2
+#define RECTANGLE_MAX_Y 3
+#define ROLE_X 0
+#define ROLE_Y 1
+#define CIRCLE_X 0
+#define CIRCLE_Y 1
+#define CIRCLE_R 3
 #include <vector>
 #include <list>
 #include <string>
 #include "attack.h"
 #include "different_unit.h"
 #include "character.h"
+enum obstacle_type
+{
+	wall,glass,water,spring
+};
 struct ATmap_obstacle{
 	int shape;
+	obstacle_type type;
 	int* shape_paramenter;
 };
 struct ATmap{
+	int limit[2];
 	std::vector<ATmap_obstacle>load_map;
 	std::vector<ATcharacter> load_charater;
 };
@@ -25,7 +40,12 @@ private:
 	std::list<ATattack> attack_information;
 	std::list<ATcharacter> character_information;
 	std::list<ATdogface> dogface_information;
-	int unit_ID;
+	std::vector<ATmap_obstacle> obstacle_information;
+	int unit_ID;//´Ó5¿ªÊ¼
+	int max_x, max_y;
+	int* judgement_rectangle(ATmap_obstacle rectangle_obstacle,int *pp,int size);
+	int* judgement_circle(ATmap_obstacle circle_obstacle,int *pp,int size);
+	int* judgement_map_edge(int *pp, int size);
 public:
 	ATjudgement(ATmap initial_map);//C
 	void judgement_add_dogface(ATdogface new_dogface);//L
