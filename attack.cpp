1c2291918a44efecc84deca01463ee78794af277
JuusanKojirou <gustavlee@126.com>
2016-05-25 16:58:34 +0800

    Add files via upload

--- attack.cpp
+++ attack.cpp
@@ -1,10 +1,9 @@
 #include "attack.h"
 
-ATattack ATattack::ATattack(AToriginal_state state, ATattack_state attack){
-	type = attack.type;
-	range = attack.range;
-	duration = attack.duration;
-	buff = attack.buff;
-	owner = attack.owner;
-	ATelements(AToriginal_state);
+ATattack::ATattack(const AToriginal_state* ori_state, ATattack_state attack_state):ATelements(ori_state){
+	type = attack_state.type;
+	range = attack_state.range;
+	duration = attack_state.duration;
+	buff = attack_state.buff;
+	belong = attack_state.belong;
 }
